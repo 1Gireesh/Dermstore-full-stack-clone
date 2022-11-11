b4159e053756db03cba995b85b2be278417caf10
Bikram Dutta <duttabikram260@gmail.com>
2022-11-11 13:51:54 +0530

    fw20_1288 day-3 added

--- frontend/dermstore/src/App.js
+++ frontend/dermstore/src/App.js
@@ -1,10 +1,16 @@
 import { Footer } from "./Components/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import AllRoutes from "./Routes/AllRoutes";
+import { ChakraProvider } from '@chakra-ui/react'
+import { Cart } from "./pages/cart_section/Cart";
+
 
 function App() {
   return (
     <div>
+      <ChakraProvider>
+      <Cart/>
+      </ChakraProvider>
       <Navbar />
       <AllRoutes />
       <Footer />
